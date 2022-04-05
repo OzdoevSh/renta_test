e9b5b86e9b9ddd0e60bdabacf3db657375675135
OzdoevSh <ozdoevsh@gmail.com>
2022-04-05 09:54:40 +0300

    finale

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 const app = express();
 
-const PORT = 3005;
+const PORT = process.env.PORT || 3005;
 
 app.use(cors())
 app.use('/api', jsonServer.router('db.json'));
