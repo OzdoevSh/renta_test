e9043ecc5dfa0abc258b77e7d85ef44ab2d9c38a
OzdoevSh <ozdoevsh@gmail.com>
2022-04-05 10:25:34 +0300

    heroku

--- client/src/store/actions/getProducts.js
+++ client/src/store/actions/getProducts.js
@@ -5,12 +5,12 @@ export const getProducts = (delivery) => async dispatch => {
   try{
     let products
     if (delivery === true){
-      products = await axios.get(`http://localhost:3005/api/products/?delivery=true`)
+      products = await axios.get(`https://rentabackendtest.herokuapp.com/api/products/?delivery=true`)
     } else {
-      products = await axios.get(`http://localhost:3005/api/products`)
+      products = await axios.get(`https://rentabackendtest.herokuapp.com/api/products`)
     }
 
-    const categories = await axios.get('http://localhost:3005/api/categories')
+    const categories = await axios.get('https://rentabackendtest.herokuapp.com/categories')
       const burgers = products.data.filter(product => categories.data[0].products.includes(product.id))
       const twisters = products.data.filter(product => categories.data[1].products.includes(product.id))
       const chicken = products.data.filter(product => categories.data[2].products.includes(product.id))
